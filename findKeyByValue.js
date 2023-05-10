c4826850e5e2e4e897cbb4a6e382c2dd50d52f87
calgarybino <binoandemeskel1234@gmail.com>
2023-05-09 23:37:37 -0400

    find a key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+const findKeyByValue = (object, value) => {
+  for (const key in object) {
+    if (object.hasOwnProperty(key) && object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(` Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(` Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
