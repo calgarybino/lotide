b0ef1e66249cef42174dbc06727ef3aa551d8714
calgarybino <binoandemeskel1234@gmail.com>
2023-06-19 20:47:23 -0400

    we create head.js and return the first index

--- head.js
+++ head.js
@@ -2,5 +2,6 @@ const assertEqual = require("./assertEqual");
 const head = function (arr) {
   return arr[0];
 };
-
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 module.exports = head;
