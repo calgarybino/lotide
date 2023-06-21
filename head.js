14cea2861c10a4c3e9cd9a20beaef43332ad6e29
calgarybino <binoandemeskel1234@gmail.com>
2023-06-20 23:30:13 -0400

    we test the first index of arr with expected value whether they are equal or not

--- head.js
+++ head.js
@@ -1,7 +1,11 @@
 const assertEqual = require("./assertEqual");
 const head = function (arr) {
-  return arr[0];
+  if (arr.length === 0) {
+    return undefined;
+  } else {
+    return arr[0];
+  }
 };
 assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), "Hello");
 module.exports = head;
