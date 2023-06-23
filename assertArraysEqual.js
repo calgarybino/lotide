62fe74200e2ee4b067b0b291e649edfbed2d8d58
calgarybino <binoandemeskel1234@gmail.com>
2023-06-22 22:47:07 -0400

    removed the test code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,11 +7,5 @@ const assertArraysEqual = function (array1, array2) {
     console.log("❌ Assertion Failed: Arrays are not equal.");
   }
 };
-const array1 = [1, 2, 3];
-const array2 = [1, 2, 3];
-assertArraysEqual(array1, array2);
-const array3 = [1, 2, 3];
-const array4 = [1, 2, 4];
-assertArraysEqual(array3, array4);
 
 module.exports = assertArraysEqual;
