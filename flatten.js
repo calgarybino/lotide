466a424f06e6a846a158920678091f479741f219
calgarybino <binoandemeskel1234@gmail.com>
2023-06-22 22:38:36 -0400

    we flatted nested array and compare with the expected output

--- flatten.js
+++ flatten.js
@@ -1,24 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
 
 const flatten = function (array) {
   let flattenedArray = [];
@@ -38,5 +19,5 @@ const inputArray = [1, 2, [3, 4], 5, [6]];
 const expectedOutput = [1, 2, 3, 4, 5, 6];
 
 const flattened = flatten(inputArray);
-assertEqual(flattened, expectedOutput);
-module.exports=flatten;
+assertEqual(eqArrays(flattened, expectedOutput), true);
+module.exports = flatten;
