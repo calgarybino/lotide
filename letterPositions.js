28abb6f5f7846fa8b87fbe3aebb98a03e5c4bb03
calgarybino <binoandemeskel1234@gmail.com>
2023-06-24 11:26:58 -0400

    comparing the position of the letter index

--- letterPositions.js
+++ letterPositions.js
@@ -1,12 +1,11 @@
 const assertArraysEqual = require("./assertArraysEqual");
-const eqArrays = require("./eqArrays");
 
-const letterPositions = function (sentence) {
+
+const letterPositions = (sentence) => {
   const results = {};
 
   for (let i = 0; i < sentence.length; i++) {
     const letter = sentence[i];
-
     if (letter !== " ") {
       if (!results[letter]) {
         results[letter] = [];
