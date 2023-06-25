1c9d6cb59487a5d57b780be593e15e48e05a7647
calgarybino <binoandemeskel1234@gmail.com>
2023-06-24 22:23:16 -0400

    comparing between two different objects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,15 +1,15 @@
 const eqArrays = require("./eqArrays");
 const eqObjects = require("./eqObjects");
-
+const inspect = require("util").inspect;
 const assertObjectsEqual = function (actual, expected) {
-  const inspect = require("util").inspect;
+  
   if (eqObjects(actual, expected)) {
     console.log(
-      ` Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+      ` ✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
     );
   } else {
     console.log(
-      `Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
+      `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
     );
   }
 };
