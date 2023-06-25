afab4998ef9a49222fc56f1fd06b3d72bbd03f88
calgarybino <binoandemeskel1234@gmail.com>
2023-06-25 15:48:02 -0400

    modifed the eqArray to work with nested array

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,35 @@
 const assertEqual = require("./assertEqual");
 const eqArrays = (array1, array2) => {
+  if (!Array.isArray(array1) || !Array.isArray(array2)) {
+    return false;
+  }
   if (array1.length !== array2.length) {
     return false;
   }
 
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+    const item1 = array1[i];
+    const item2 = array2[i];
+    // If both items are arrays, recursively check their equality
+    if (Array.isArray(item1) && Array.isArray(item2)) {
+      if (!eqArrays(item1, item2)) {
+        return false;
+      }
+    } else if (item1 !== item2) {
       return false;
     }
   }
   return true;
 };
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(
+//   eqArrays(
+//     [[2, 3], [4]],
+//     [
+//       [2, 3],
+//       [4, 5],
+//     ]
+//   ),
+//   true
+// );
 module.exports = eqArrays;
